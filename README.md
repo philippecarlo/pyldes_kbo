970d2911b4a4b712b226b5e5599d2e3a3adf83a0
Xueying Deng (imec) <deng49@imec.be>
2023-03-02 16:51:39 +0100

    Fixed typos in the Read me, and make the 'version_bel20' option
    Fixed typos in the Read me, and make the version_bel20 option

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # [Crossroads Bank for Enterprises](https://economie.fgov.be/en/themes/enterprises/crossroads-bank-enterprises/services-everyone/public-data-available-reuse/cbe-open-data) published as LDES
 # How To Run
 
-## The python project could be run with following arguments:
+##  The python project could be run with the following arguments:
 \
-    - ```load``` : Which load the csv files from the given repository defined by ```VERSION``` to a db files defined by ``DB_LOCATION``\
-    - ```sample``` : Which gives a nonversioned KBO Enterprise Enrity\
-    - ```run``` : Which gives all nonversioned KBO Enterprise Entities \
-    - ```version_sample``` : Which gives a versioned KBO Enterprise Enrity\
-    - ```version_sun``` : Which gives all versions KBO Enterprise Entity\
+    - ```load```: Which loads the CSV files from the given repository defined by ```VERSION``` to a DB file defined by ``DB_LOCATION``\
+    - ```sample```: Which gives a nonversioned KBO Enterprise Entity\
+    - ```run```: Which gives all nonversioned KBO Enterprise Entities \
+    - ```version_sample```: Which gives a versioned KBO Enterprise Entity\
+    - ```versionr_run```: Which gives all versioned KBO Enterprise Entities\
+    - ```version_bel20```: Which gives versioned [Bel20](https://tradingeconomics.com/belgium/stock-market) companies
--- app.py
+++ app.py
@@ -18,9 +18,30 @@ VERSION = "KboOpenData_2022_11"
 DB_LOCATION = "data/kbo.db"
 BASE_PATH='data'
 
+BEL_20=["0417.497.106",
+        "0404.616.494",
+        "0877.248.501",
+        "0451.406.524",
+        "0401.277.914",
+        "0818.292.196",
+        "0426.184.049",
+        "0400.378.485",
+        "0403.448.140",
+        "0476.388.378",
+        "0466.460.429",
+        "0407.040.209",
+        "0403.227.515",
+        "0202.239.951",
+        "0403.219.397",
+        "0403.091.220",
+        "0403.053.608",
+        "0401.574.852",
+        "0887.216.042",
+        "0417.199.869"]
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run the KBO example.')
-    parser.add_argument('command', type=str, choices=['load', 'run', 'sample','version_sample','version_run'], help='Command to run')
+    parser.add_argument('command', type=str, choices=['load', 'run', 'sample','version_sample','version_run','version_bel20'], help='Command to run')
     args = parser.parse_args()
     if args.command == 'load':
         loader= DataLoader(BASE_PATH, VERSION, DB_LOCATION)
@@ -101,3 +122,28 @@ if __name__ == '__main__':
             print(graph.serialize(format='turtle'))
             # print(f"Processing record {current_rec} of {total_records} [{current_rec/total_records*100:.2f}%]\r", end="")
             print(f"Processing record {current_rec} of {total_records} [{current_rec / total_records * 100:.2f}%]")
+
+    elif args.command == 'version_bel20':
+
+        total_records = len(BEL_20)
+        current_rec = 0
+        for enterprise in BEL_20:
+             current_rec = current_rec + 1
+             enterprise = KboGenerator(BASE_PATH, DB_LOCATION).one(enterprise)
+             graph = Graph()
+
+             # bind kbo, org, foaf, vard prefix.
+             graph.bind("kbo", KBO)
+             graph.bind("org", ORG)
+             graph.bind("foaf", FOAF)
+             graph.bind("vcard", VCARD)
+             graph.bind("locn", LOCN)
+             graph.bind("legal", LEGAL)
+             graph.bind("terms", TERMNAME)
+
+             enterprise.to_rdf_version(graph, with_blank_nodes=False)
+             ent_dict = enterprise.to_dict()
+             # print(json.dumps(ent_dict, indent=4))
+             # print(ent_dict)
+             print(graph.serialize(format='turtle'))
+             print(f"Processing record {current_rec} of {total_records} [{current_rec / total_records * 100:.2f}%]")

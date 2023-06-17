de703f99c7bba17aebace0105471cccfcde7bd60
XD <xueying_deng@outlook.com>
2023-06-17 23:33:53 +0200

    Update test sub

--- ctesttree/testsub/kbolaunch/config/seed.sh
+++ ctesttree/testsub/kbolaunch/config/seed.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-curl -X POST 'http://localhost:8080/admin/api/v1/eventstreams' -H 'Content-Type: text/turtle' -d '@./kbo.ttl'
+#Start docker containers
+docker compose up -d
+
+#Wwait the system starts
+sleep 2m
+
+#Post the stream/view configuration
+curl -X PUT 'http://localhost:8080/admin/api/v1/eventstreams' -H 'Content-Type: text/turtle' -d '@./kbo.ttl'
 if [ $? != 0 ] 
     then exit $? 
 fi
 
-curl -X POST 'http://localhost:8080/admin/api/v1/eventstreams/mobility-hindrances/views' -H 'Content-Type: text/turtle' -d '@./kbo.timefragments.ttl'
-if [ $? != 0 ] 
-    then exit $? 
-fi
+#Post dataset
+for f in ../../../../sample/bel20/*; do curl -i -X POST "http://localhost:8080/kbo" -H "Content-Type: application/turtle" -d "@$f";done
